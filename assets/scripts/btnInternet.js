66af513403c16087a9a2a0ec5dbe477b46fa7299
xulipaxulepa <aofnas@hotmail.com>
2017-04-18 10:41:19 -0300

    Sistema inicial do jogo completo

---
+++ assets/scripts/btnInternet.js
@@ -0,0 +1,48 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        prof1: {
+            default: null,
+            type: cc.Node
+        },
+        
+        prof2: {
+            default: null,
+            type: cc.Node
+        },
+        
+        prof3: {
+            default: null,
+            type: cc.Node
+        },
+        
+        prof4: {
+            default: null,
+            type: cc.Node
+        },
+        
+        button: {
+            default: null,
+            type: cc.Node
+        }
+    },
+    
+    showProfButton: function(){
+        this.prof1.setOpacity(0);
+        this.prof2.setOpacity(1000);
+        this.prof3.setOpacity(0);
+        this.prof4.setOpacity(0);
+        this.button.setPosition(326, -244);
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
